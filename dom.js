bec675c01bff89f6f32954057af566fbe0cbaf11
ALTGAME <krgpsh13@gmail.com>
2023-08-08 02:03:00 +0530

    index addition

---
+++ dom.js
@@ -0,0 +1,22 @@
+//EXAMINE THE DOCUMENT OBJECT//
+
+//console.dir(document);
+//console.log(document.title);
+//console.log(document.URL);
+//console.log(document.domain);
+//console.log(document);
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.all);
+//console.log(document.all[10]);
+//document.all[10].textContent='HELLo';
+//console.log(document.forms);
+//console.log(document.links);
+
+//GETELEMENTBYID
+
+//console.log(document.getElementById('header-title'));
+//var headerTitle = document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent="HELLO";
+//headerTitle.innerText="ITS HELL";
