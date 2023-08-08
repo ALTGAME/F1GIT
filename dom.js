746aaf8e589234067e03bcd4f4e00b0ac3406766
ALTGAME <krgpsh13@gmail.com>
2023-08-08 12:20:17 +0530

    GETELEMENTSBYCLASSNAME

--- dom.js
+++ dom.js
@@ -20,3 +20,18 @@
 //console.log(headerTitle);
 //headerTitle.textContent="HELLO";
 //headerTitle.innerText="ITS HELL";
+
+//GETELELMENTSBYCLASSNAME
+
+var items= document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent= 'TROLL';
+items[1].style.fontWeight= 'bold';
+items[2].style.backgroundColor='green';
+
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight= 'bold';
+} 
+
