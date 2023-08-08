06cea55dffd1d530dfdb2a9c192a5757156e7ce7
ALTGAME <krgpsh13@gmail.com>
2023-08-08 12:56:21 +0530

    TAGNAME

--- dom.js
+++ dom.js
@@ -23,15 +23,30 @@
 
 //GETELELMENTSBYCLASSNAME
 
-var items= document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent= 'TROLL';
-items[1].style.fontWeight= 'bold';
-items[2].style.backgroundColor='green';
+//var items= document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
+//items[1].textContent= 'TROLL';
+//items[1].style.fontWeight= 'bold';
+//items[2].style.backgroundColor='green';
 
 
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight= 'bold';
+//for(var i=0;i<items.length;i++){
+  //  items[i].style.fontWeight= 'bold';
+//} 
+
+
+//GETELEMENTSBYTAGNAME
+
+var li= document.getElementsByTagName('list-group-item');
+console.log(li);
+console.log(li[1]);
+li[1].textContent= 'TROLL';
+li[1].style.fontWeight= 'bold';
+li[2].style.backgroundColor='green';
+
+
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight= 'bold';
 } 
 
